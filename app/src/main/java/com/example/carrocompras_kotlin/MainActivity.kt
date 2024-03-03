ef87d92bd02393e152de91dccc19c72387b419e3
jr1ochoa <joserrobert8a@gmail.com>
2024-03-03 10:01:20 -0600

    Carrito

--- app/src/main/java/com/example/carrocompras_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/carrocompras_kotlin/MainActivity.kt
@@ -30,10 +30,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addproducts() {
-        productlist.add(Producto("101", "Product No 1", "Made with natural ingredients", 40.00))
-        productlist.add(Producto("102", "Product No 2", "Made with premium materials", 30.00))
-        productlist.add(Producto("103", "Product No 3", "Wooden chair made with natural materials", 90.00))
-        productlist.add(Producto("104", "Product No 4", "Made with natural ingredients", 50.00))
-        productlist.add(Producto("105", "Product No 5", "Brand new high heels shoes", 80.00))
+        productlist.add(Producto("101", "Sansung S24 Ultra", "Do What You Can't", 1400.00))
+        productlist.add(Producto("102", "Iphone 15 Pro Max", "Think Differen", 1299.00))
+        productlist.add(Producto("103", "Huawei P60 Pro", "Make It Possible ", 1100.00))
+        productlist.add(Producto("104", "Google Pixel 8 Pro", "Don't be evil", 999.00))
+        productlist.add(Producto("105", "Nothing Phone 2", "Impossible is nothing", 699.00))
     }
 }
